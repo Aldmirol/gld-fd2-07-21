c1bd48be78c937f1bf423d9712162788d0f9342b
Aliaksei Prykhodzka <70562140+Aldmirol@users.noreply.github.com>
2021-02-09 23:04:57 +0300

    Update task-07-08.js

--- js/functions/basics/task-07-08.js
+++ js/functions/basics/task-07-08.js
@@ -2,24 +2,26 @@
 которые меньше 100 и отсортируйте его по возрастанию.Выведите результат в консоль.
 Создайте конструкцию, с помощью которой сразу выполниться выше реализованная функция conc.Используйте IIFE.*/
 
-const arr = [1, 8, 3, 5, 12, 7, 9, 11];
+(function() {
+    const arr = [1, 8, 3, 5, 12, 7, 9, 11];
 
-const multArrElem = arr => {
-    let arrSort = [];
+    const multArrElem = arr => {
+        let arrSort = [];
 
-    for (let i = 0; i < arr.length;i++) {
-        let elem = arr[i] * arr[i - 1];
+        for (let i = 0; i < arr.length; i++) {
+            let elem = arr[i] * arr[i - 1];
 
-        if (elem < 100) {
-            arrSort.push(elem);
+            if (elem < 100) {
+                arrSort.push(elem);
+            }
         }
-    }
 
-    arrSort.sort(function(a, b) {
-        return a - b;
-    });
+        arrSort.sort(function (a, b) {
+            return a - b;
+        });
 
-    console.log(arrSort);
-};
+        console.log(arrSort);
+    };
 
-multArrElem(arr);
+    multArrElem(arr);
+})();
