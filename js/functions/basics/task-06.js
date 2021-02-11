910b8f30d0453b0ec3c4fb6c9ba72042a06ab98f
Aliaksei Prykhodzka <70562140+Aldmirol@users.noreply.github.com>
2021-02-11 10:47:45 +0200

    Update task-06.js

--- js/functions/basics/task-06.js
+++ js/functions/basics/task-06.js
@@ -2,10 +2,9 @@
 которые представляют список параметров и тело будущей функции.
 Создайте функцию, используя эту информацию с помощью Function Constructor(FC).Вызовите эту функцию.*/
 
-const paramFirst = 'a';
-const paramSecond = 'b';
+const params = 'a, b';
 const body = 'return a * b';
 
-const mult = new Function(paramFirst, paramSecond, body);
+const mult = new Function(params, body);
 
 console.log(mult(6, 5));
